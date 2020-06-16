2633f3766d3b53deeb6df700652fc23b5f642a15
Tristan Guest <tristanguest@gmail.com>
2020-06-16 13:32:13 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -8,5 +8,5 @@ module.exports = { name, height
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
-  console.log("Expected the name to be Susan")
+  console.log("Error: Expected the name to be Susan")
 }
